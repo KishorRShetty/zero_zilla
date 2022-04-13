c272d3480f20cdce34578da1f751e593500efcf9
KishorRShetty <kishhlinuxshetty@gmail.com>
2022-04-13 07:34:05 +0000

    byIdDone

--- jd_shop/src/ProductById/ProductById.jsx
+++ jd_shop/src/ProductById/ProductById.jsx
@@ -7,29 +7,51 @@ import "./ProductById.css";
 function ProductById() {
   const { id } = useParams();
   const { count, setCount } = useContext(CartContext);
-  const { product, setProduct } = useState([]);
+  const [product, setProduct] = useState([]);
+
+  // useEffect(() => {
+  // fetch('https://fakestoreapi.com/products/1')
+  //         .then(res=>res.json())
+  //         .then(json=>setProduct(json))
+  //         .then(json=>console.log(json))
+
+  // async function fetchProduct() {
+  //   console.log(`inside useEffectId${id}`);
+  //   console.log(`https://fakestoreapi.com/products/${id}`);
+  //   const requestProduct = await axios.get(`https://fakestoreapi.com/products/${id}`);
+  //   setProduct(requestProduct.data);
+  //   console.log(requestProduct);
+  //   return requestProduct;
+  // }
+  // fetchProduct();
+  // }, []);
 
   useEffect(() => {
-    async function fetchProduct() {
-      console.log(`inside useEffectId${id}`);
-      console.log(`https://fakestoreapi.com/products/${id}`);
-      const requestProduct = await axios.get(`https://fakestoreapi.com/products/${id}`);
-      setProduct(requestProduct.data);
-      console.log(requestProduct);
-      return requestProduct;
+    async function getProduct() {
+      const prodData = await axios.get(
+        `https://fakestoreapi.com/products/${id}`
+      );
+      // console.log(prodData.data);
+      setProduct(prodData.data);
+      return prodData;
     }
-    fetchProduct();
+    getProduct();
   }, []);
 
-  return (
-    <>
-      <br />
-      <br />
-      <div className="top">
-        ProductById {id}
-        {/* <p>{product.title}</p> */}
-      </div>
-    </>
+  return (<><br/>
+  <div className="top wrap">
+    {/* {console.log(product)} */}
+    <div className="detailsCard">
+      <img src={product.image} alt="Prod" style={{ width: "100%" }} />
+      <h1>{product.title}</h1>
+      <p className="detailsPrice">${product.price}</p>
+      <p>{product.description}
+      </p>
+      <button onClick={()=>setCount(count+1)}>Add to Cart</button>
+      
+    </div>
+  </div>
+</>
   );
 }
 
