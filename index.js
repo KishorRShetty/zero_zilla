4f3ba0c59a67f6515ed2154596eb6421eb5a3005
KishorRShetty <kishhlinuxshetty@gmail.com>
2022-04-13 14:08:11 +0000

    added server

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+// const cors = require('cors');
+const path = require('path');
+
+app.use(express.json()); //parsing json data from the body
+// app.use(cors());
+
+// for Hosting on heroku
+app.use(express.static(path.join(__dirname,"./jd_shop/build")));
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./jd_shop/build/index.html"));
+})
+// for Heroku
+
+
+app.listen(4000, () => {
+    console.log(`Server is running on 4000`);
+  });
